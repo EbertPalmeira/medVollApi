61ce71e8b88dc0ad9f6d331cf4452c6d4824381e
Ebert Palmeira <eberttpalmerass@gmail.com>
2026-05-04 11:43:04 -0300

    adicionando metodo get

--- src/main/java/med/voll/api/controller/PacienteController.java
+++ src/main/java/med/voll/api/controller/PacienteController.java
@@ -1,14 +1,14 @@
 package med.voll.api.controller;
 
 import jakarta.validation.Valid;
+
 import med.voll.api.paciente.DadosCadastroPaciente;
 import med.voll.api.paciente.Paciente;
 import med.voll.api.repository.PacienteRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("paciente")
@@ -21,4 +21,9 @@ public class PacienteController {
     public void cadastrar(@RequestBody @Valid DadosCadastroPaciente dados){
         repository.save(new Paciente(dados));
     }
+
+
+
+
+
 }
